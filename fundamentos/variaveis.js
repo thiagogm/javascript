6ed4dd29aeb7b670b3493369f528b19ff57b859f
Thiago Moura <thiagoguimamoura@hotmail.com>
2024-09-13 21:33:57 -0300

    estudo de variáveis

---
+++ fundamentos/variaveis.js
@@ -0,0 +1,23 @@
+/**
+ * Estudo de constante e variáveis
+ * @author Thiago Moura
+ */
+
+console.clear()
+
+//variáveis
+let nome
+let idade
+let vip
+
+nome = "Thiago Moura"
+console.log(typeof(nome))
+console.log(nome)
+
+idade = 56
+console.log(typeof(idade))
+console.log(idade)
+
+vip = true
+console.log(typeof(vip))
+console.log(vip)
