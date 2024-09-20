d66c8b2a8aa5c59f41d23af6bb4ed32d17aeb57f
Thiago Moura <thiagoguimamoura@hotmail.com>
2024-09-19 22:18:22 -0300

    Exerício Ficha do aluno Academia

---
+++ exercicios/academia.js
@@ -0,0 +1,30 @@
+/** 
+ * Ficha de um aluno de academia
+ * * author Thiago Moura
+ */
+
+//variáveis
+let nome
+let idade
+let peso
+let altura
+let vip
+
+console.clear()
+
+// entrada de dados
+nome = "Thiago Moura"
+idade = 39
+peso = 115
+altura = 1.95
+vip = true
+
+// saída
+console.log("Ficha do aluno")
+console.log("_______________________________")
+console.log(`Nome: ${nome}`)
+console.log(`Idade: ${idade}`)
+console.log(`Peso: ${peso}`)
+console.log(`Altura: ${altura}`)
+console.log(`VIP: ${vip}`)
+
