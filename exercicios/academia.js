37fe06623203ac90852f9313ed802f3e8cad64b9
Thiago Moura <thiagoguimamoura@hotmail.com>
2024-09-26 19:16:55 -0300

    Atualização

--- exercicios/academia.js
+++ exercicios/academia.js
@@ -9,7 +9,8 @@ let idade
 let peso
 let altura
 let vip
-
+let fcm
+let imc
 console.clear()
 
 // entrada de dados
@@ -19,6 +20,10 @@ peso = 115
 altura = 1.95
 vip = true
 
+//processamento
+fcm = 208 - (0.7 * idade)
+imc = peso / (altura * altura)
+
 // saída
 console.log("Ficha do aluno")
 console.log("_______________________________")
@@ -27,4 +32,5 @@ console.log(`Idade: ${idade}`)
 console.log(`Peso: ${peso}`)
 console.log(`Altura: ${altura}`)
 console.log(`VIP: ${vip}`)
-
+console.log(`FCM:  ${fcm}`)
+console.log(`IMC:  ${imc.toFixed(2)}`)
