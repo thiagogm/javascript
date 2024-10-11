324a74fd02669fad5c6a40efa2524765b7ba4235
Thiago Moura <thiagoguimamoura@hotmail.com>
2024-10-10 22:13:33 -0300

    operadores lógicos

---
+++ fundamentos/operadoresLogicos.js
@@ -0,0 +1,21 @@
+
+const input = require('readline-sync')
+
+let idade 
+
+console.clear()
+console.log("Verificar maioridade")
+idade = Number(input.question("Digite a idade: "))
+console.log(`Idade ${idade}`)
+
+if (idade < 18){
+    console.log("Menor de idade")
+}
+
+if (idade === 18){
+    console.log("Maior de idade")
+}
+
+if (idade > 18){
+    console.log("Maior de idade")
+}
